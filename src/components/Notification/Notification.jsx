421423d213e6a5c44f1345ab0b603dc8d012e6b7
With0utN3me <sinitsin678@gmail.com>
2024-04-07 21:49:17 +0200

    base

--- src/components/Notification/Notification.jsx
+++
@@ -1,7 +0,0 @@
-import css from "./Notification.module.css"
-const Notification = ({text}) => {
-    return (
-        <p className={css["notification"]}>{text}</p>
-    )
-}
-export default Notification;
